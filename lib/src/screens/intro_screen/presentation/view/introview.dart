cda1e19558ce74513e0c779a26e063948a8c174f
tishey <ambalisulaimon4@gmail.com>
2023-01-20 23:23:19 +0100

    Added new and latest screens and did some new changes about routing

--- lib/src/screens/intro_screen/presentation/view/introview.dart
+++ lib/src/screens/intro_screen/presentation/view/introview.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qwiyi_flutter/src/screens/onboarding/view/onboarding.dart';
 import 'package:qwiyi_flutter/src/share/color.dart';
 import 'package:qwiyi_flutter/src/share/function.dart';
@@ -19,6 +18,15 @@ class _IntroViewState extends State<IntroView>
   late final AnimationController controller =
       AnimationController(vsync: this, duration: const Duration(seconds: 2))
         ..forward();
+    
+
+    
+  @override
+  void initState(){
+      Timer(const Duration(seconds: 5), () {
+   NavigatorRoute.navigateAndRemoveUntilRoute(const OnboardingScreeen());
+    });
+  }
 
   @override
   void dispose() {
@@ -26,10 +34,10 @@ class _IntroViewState extends State<IntroView>
     super.dispose();
   }
 
+
   @override
   Widget build(BuildContext context) {
     Timer(const Duration(seconds: 5), () {
-   
    NavigatorRoute.navigateToRoute(const OnboardingScreeen());
     });
     // ScreenUtil.init(context);
--- lib/src/screens/onboarding/view/onboarding.dart
+++ lib/src/screens/onboarding/view/onboarding.dart
@@ -24,7 +24,7 @@ class OnboardingScreeen extends StatelessWidget {
             children: [
               Text('Qwiyi',
                   style:
-                      QwiyiTypography.bigPrimaryTextStyle(context).copyWith(fontSize: 36.sp)),
+                  QwiyiTypography.bigPrimaryTextStyle(context).copyWith(fontSize: 36.sp)),
               Container(
                 height: screenAwareSize(365, context),
                 width: screenAwareSize(235, context, width: true),
@@ -42,7 +42,6 @@ class OnboardingScreeen extends StatelessWidget {
                       press: () {
                       NavigatorRoute.navigateToRoute(const SignInScreen());
                       },
-                         
                       text: 'Sign In',
                       width: 150,
                       textSize: 18.sp),
